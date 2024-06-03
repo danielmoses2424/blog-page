2c028a19099c7c1daaf3b7cd352a4055c02234e4
daniel moses <md1004951gmail.com>
2024-06-03 14:32:04 +0100

    blog-page

--- src/firebase-configue.js
+++ src/firebase-configue.js
@@ -4,7 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA56aqq78sJwT-e1MWPkBFAUIh6uL43bHk",
+  apiKey: import.meta.env.VITE_API_KEY,
   authDomain: "blog-3eccc.firebaseapp.com",
   projectId: "blog-3eccc",
   storageBucket: "blog-3eccc.appspot.com",
